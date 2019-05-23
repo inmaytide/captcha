8c3b3d40d1b6812d9942030ceb5b5c072e26f36f
inmaytide <inmaytide@gmail.com>
2019-05-23 13:11:15 +0800

    first commit

---
+++ cache/store.go
@@ -0,0 +1,42 @@
+package cache
+
+import "log"
+
+const CACHE_EXPIRE = 15 * 60
+
+type CaptchaStore struct {
+	client *RedisClient
+}
+
+func NewCaptchaStore(client *RedisClient) *CaptchaStore {
+	return &CaptchaStore{client}
+}
+
+func (s *CaptchaStore) Set(id string, value string) {
+	err := s.client.ESet(id, value, CACHE_EXPIRE)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *CaptchaStore) Get(id string, clear bool) string {
+	log.Println(id);
+
+	value, err := s.client.Get(id)
+	if err != nil {
+		panic(err)
+	}
+
+	if clear {
+		err := s.client.Delete(id)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	result := string(value)
+
+	log.Println(result)
+
+	return result[1:len(result)-1];
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module captcha
+
+go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.2
+	github.com/mediocregopher/radix/v3 v3.2.3
+	github.com/mojocn/base64Captcha v0.0.0-20190509095025-87c9c59224d8
+	github.com/satori/go.uuid v1.2.0
+	go.uber.org/dig v1.7.0
+	gopkg.in/yaml.v2 v2.2.2
+)
